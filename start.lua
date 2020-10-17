d99c547d5f43f12b08dd01c421d4c14a52ea1f8a
Jon <kittle31@gmail.com>
2020-10-16 18:36:07 -0700

    initial commit

---
+++ start.lua
@@ -0,0 +1,5 @@
+screen.activate()
+screen.clear()
+indexes = {}
+unit.setTimer("Monitor", 5)
+screen.addContent(1, 0, "<div style=\"text-align: center; font-size: 12vh; border-bottom: 3px solid white; width: 100vw\">Ingots</div>")
